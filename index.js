af8268463bf366e381e362ef85425e5e94df5490
ataki <jim32990@gmail.com>
2018-03-08 09:23:49 -0800

    get docs from all collections

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ firebase.initializeApp(require('./firebase.config.json'));
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-let query = db.collection('operations');
-query.get().then(function(snap) {
-    let data = snap.docs.map(x => JSON.stringify(x.data()) );
-    fs.writeFile('./backup.operations.json', data.join('\n'), (err) => {
-        if (err) console.error(err);
-        else console.log(data.length + ' records written.')
+const queries = {
+    'operations': db.collection('operations'),
+    'members': db.collection('members'),
+    'relations': db.collection('relations'),
+    'user_data': db.collection('user_data')
+};
+
+Object.entries(queries).forEach((nmQry) => {
+    let [name, query] = nmQry;
+    query.get().then(function(snap) {
+        let data = snap.docs.map(x => JSON.stringify(x.data()) );
+        fs.writeFile('./backup.' + name + '.json', data.join('\n'), (err) => {
+            if (err) console.error(err);
+            else console.log(data.length + ' records written for ' + name + '.')
+        });
     });
 });
